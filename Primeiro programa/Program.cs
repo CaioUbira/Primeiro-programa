447df82b4fd6c616d5f49becea06075c9f79d920
Caio Ubira <caio_ubirajara@outlook.com>
2026-08-17 22:58:14 -0300

    Atualiza projeto

--- Primeiro programa/Program.cs
+++ Primeiro programa/Program.cs
@@ -1,12 +1,28 @@
 ﻿// Screen Sound 
-//camelcase
-string mensagemDeBoasVindas = "Boas Vindas ao Screen Sound";
-//
-void ExibirMesagemDeBoasVindas()
-{
-    Console.WriteLine("************************");
-    Console.WriteLine(mensagemDeBoasVindas);
-    Console.WriteLine("************************");
 
+//camelcase A primeira palavra começa com letra minúscula e as próximas começam com maiúscula.
+string mensagemDeBoasVindas = "Boas Vindas ao Screen Sound";
+
+//PascalCase todas as palavras começam com letra maiúscula, inclusive a primeira.
+void ExibirMesagemDeBoasVindas()
+{// Verbatim Litera É especialmente útil para caminhos de arquivos, textos com muitas \ e strings multilinha.
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+
+    Console.WriteLine(mensagemDeBoasVindas\n);
+}
+void ExibirOpçoesDoMeni()
+{
+    Console.WriteLine("\nDigite 1 para registarr uma banda ");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite 5 para sair ");
 }
 ExibirMesagemDeBoasVindas();
+ExibirOpçoesDoMeni();
