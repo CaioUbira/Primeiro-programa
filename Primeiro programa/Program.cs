521d73fcc707f0ef8a694545a4abb6acbe70f1cd
Caio Ubira <caio_ubirajara@outlook.com>
2026-09-16 15:42:10 -0300

    feat: adiciona menu inicial do Screen Sound

--- Primeiro programa/Program.cs
+++ Primeiro programa/Program.cs
@@ -14,7 +14,7 @@ void ExibirMesagemDeBoasVindas()
 ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
 ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
 
-    Console.WriteLine(mensagemDeBoasVindas\n);
+    Console.WriteLine(mensagemDeBoasVindas); 
 }
 void ExibirOpçoesDoMeni()
 {
@@ -23,6 +23,23 @@ void ExibirOpçoesDoMeni()
     Console.WriteLine("Digite 3 para avaliar uma banda");
     Console.WriteLine("Digite 4 para exibir a média de uma banda");
     Console.WriteLine("Digite 5 para sair ");
+    Console.Write("\nDigite a sua opção: ");
+
+    /* Esse ! é o operador de supressão de nulo (null-forgiving operator). 
+     * O Console.ReadLine() pode retornar null, e o C# avisa sobre isso dependendo das configurações de nullable reference types.
+     * O !está dizendo ao compilador:"Eu sei que esse valor pode ser nulo, mas neste caso pode confiar que não será." */
+    string opcaoEscolhida = Console.ReadLine()!;
+
+    //  "Parse" serve para converter o texto (string) que o usuário digitou em um número inteiro
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+    if (opcaoEscolhidaNumerica == 1)
+    {
+        Console.WriteLine(" Voce escolhe a opção " + opcaoEscolhidaNumerica);
+    }
+    else if (opcaoEscolhidaNumerica == 2)
+    {
+        Console.WriteLine(" Voce escolhe a opção " + opcaoEscolhidaNumerica);
+    }
 }
 ExibirMesagemDeBoasVindas();
 ExibirOpçoesDoMeni();
